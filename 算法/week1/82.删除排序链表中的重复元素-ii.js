bb095b18a60c536c2b26450258899ffc029d45b8
frllk <1402805300@qq.com>
2022-01-12 23:54:50 +0800

    feat: 82 83

--- 算法/week1/82.删除排序链表中的重复元素-ii.js
+++ 算法/week1/82.删除排序链表中的重复元素-ii.js
@@ -17,7 +17,21 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-
+  if (!head) return null;
+  let ret = new ListNode(-1, head), pre = ret, cur = head;
+  while (cur && cur.next) {
+    if (pre.next.val !== cur.next.val) {
+      cur = cur.next;
+      pre = pre.next;
+    } else {
+      while (cur && cur.next && pre.next.val === cur.next.val) {
+        cur = cur.next;
+      }
+      pre.next = cur.next;
+      cur = cur.next;
+    }
+  }
+  return ret.next;
 };
 // @lc code=end
 
--- 算法/week1/83.删除排序链表中的重复元素.js
+++ 算法/week1/83.删除排序链表中的重复元素.js
@@ -17,7 +17,19 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-
+  if (!head) return null;
+  let pre = head, cur = head.next;
+  while (cur) {
+    if (pre.val != cur.val) {
+      pre.next = cur;
+      pre = cur;
+      cur = cur.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+  pre.next = null; 
+  return head;
 };
 // @lc code=end
 
