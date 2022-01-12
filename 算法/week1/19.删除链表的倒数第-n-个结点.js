0bafc310ebbf5badbe1bcc6ef320143fb722b4f9
frllk <1402805300@qq.com>
2022-01-12 22:38:22 +0800

    feat: 19,24

--- 算法/week1/19.删除链表的倒数第-n-个结点.js
+++ 算法/week1/19.删除链表的倒数第-n-个结点.js
@@ -1,4 +1,12 @@
 /*
+ * @Author: your name
+ * @Date: 2022-01-09 23:08:39
+ * @LastEditTime: 2022-01-12 22:31:48
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \frllk-studybook\算法\week1\19.删除链表的倒数第-n-个结点.js
+ */
+/*
  * @lc app=leetcode.cn id=19 lang=javascript
  *
  * [19] 删除链表的倒数第 N 个结点
@@ -18,7 +26,20 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-
+  // head是否为空
+  if (!head) return null;
+  // 创建一个虚头，创建两个指针，一个指向虚头pre，一个指向真实头结点cur
+  let ret = new ListNode(-1, head), pre = ret, cur = head;
+  // 让cur移动K步
+  for (let i = 0; i < n; i++) {
+    cur = cur.next;
+  }
+  if (!cur) return head.next;
+  // 然后让两个指针一起移动，直到cur指向空
+  while (cur) cur = cur.next, pre = pre.next;
+  // 然后进行删除操作
+  pre.next = pre.next.next;
+  return ret.next;
 };
 // @lc code=end
 
--- 算法/week1/24.两两交换链表中的节点.js
+++ 算法/week1/24.两两交换链表中的节点.js
@@ -1,4 +1,12 @@
 /*
+ * @Author: your name
+ * @Date: 2022-01-09 23:08:39
+ * @LastEditTime: 2022-01-11 00:18:25
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \frllk-studybook\算法\week1\24.两两交换链表中的节点.js
+ */
+/*
  * @lc app=leetcode.cn id=24 lang=javascript
  *
  * [24] 两两交换链表中的节点
@@ -16,8 +24,21 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var swapPairs = function(head) {
-
+var swapPairs = function (head) {
+  // 判断head是否为空
+  if (!head) return null
+  // 将相邻的两个链表进行反转
+  let ret = new ListNode(-1, head), temp = ret;
+  while (temp.next && temp.next.next) {
+    let pre = temp.next, cur = temp.next.next;
+    // pre.next = cur.next;
+    // cur.next = pre;
+    // temp.next = cur;
+    // temp = pre;
+    [pre.next, cur.next, temp.next, temp] = [cur.next, pre, cur, pre];
+  }
+  // 然后返回链表
+  return ret.next;
 };
 // @lc code=end
 
