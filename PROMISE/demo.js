08ac0579bc48db3f02b54a07a042140dbf2b75ad
frllk <1402805300@qq.com>
2022-01-20 01:00:37 +0800

    fix: promise学习

--- PROMISE/demo.js
+++ PROMISE/demo.js
@@ -1,20 +1,30 @@
 /*
  * @Author: your name
  * @Date: 2021-12-27 21:00:30
- * @LastEditTime: 2022-01-05 07:49:46
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-01-20 00:41:43
+ * @LastEditors: frllk
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \PROMISE-PRO\demo.js
+ * @FilePath: \frllk-studybook\PROMISE\demo.js
  */
 // // 高阶函数：函数的参数中有函数（一个函数接收一个函数参数），或者是像闭包一样返回一个函数
-// // function doSth(t, cb) {
-// //   return function () {
-// //     if (--t === 0) {
-// //       cb()
-// //     }
-// //   }
-// // }
-
+// /**
+//  * @description 执行doSth方法几次之后，去执行cb
+//  * @param {*} t 次数
+//  * @param {*} cb 回调
+//  */
+// function doSth(t, cb) {
+//   return function () {
+//     if (--t === 0) {
+//       cb()
+//     }
+//   }
+// }
+// /**
+//  * @description 将logSth2方法作为cb执行
+//  * @event:
+//  * @param {*} cb
+//  * @return {*}
+//  */
 // function logSth(cb) {
 //   console.log('我终于找到女朋友了！呀呼~~~');
 //   cb()
@@ -31,7 +41,7 @@
 
 // // let fn = doSth(4, logSth.bind(null, logSth2))
 
-// // 回调地狱
+// // 回调地狱：不好维护，函数里面是可以无限往下走的
 // function doSth(t) {
 //   return function () {
 //     if(--t === 0) {
@@ -45,7 +55,7 @@
 // }
 
 // let fn = doSth(4, logSth)
-
+// // fn调用4次之后才会执行
 // fn()
 // fn()
 // fn()
@@ -113,21 +123,62 @@
 // })
 // console.log(2);
 
+// executor是同步执行的
 let promise = new Promise((resolve, reject) => {
   // resolve('承诺实现')
-  // reject('承诺石沉大海')
-  throw new Error('error：承诺石沉大海')
+  // reject('承诺失败')
+  // 直接抛出一个错误的话，reject和catch也是可以拿到的
+  throw new Error('error：承诺失败')
 })
 
-// then是异步调用
-// then reject具有穿透作用
+// // then是异步调用
+// promise.then((res) => {
+//   console.log('then resolve')
+// }, (err) => {
+//   console.log('then reject', err)
+// })
+
+// console.log('window')
+
+// /**
+//  * @description then返回一个新的Promise实例
+//  */
+// promise.then((res) => {
+//   // 参数res是resolve出来的结果
+//   console.log('then resolve', res);
+//   // 这里还是需要返回一个Promise，后面的then才能继续使用
+//   return new Promise((resolve, reject) => resolve(res+'111'))
+// }, (res) => {
+//   // 只返resolve时，这里是不会走的
+//   console.log(1, res)
+// }).then((res) => {
+//   // 此处的res需要上一级then返回Promise对象
+//   console.log(2, res);
+// })
+
+/**
+ * @description then返回一个新的Promise实例，then是异步调用
+ */
 promise.then((res) => {
-  console.log('1');
-}).then((res) => {
-  
-}, (err) => {
-  console.log('then', err);
-}).catch((err) => {
+  // 参数res是resolve出来的结果
+  console.log('then resolve', res);
+  // 这里还是需要返回一个Promise，后面的then才能继续使用
+  // return new Promise((resolve, reject) => resolve(res+'111'))
+}
+//   , (res) => {
+//   // 只返resolve时，这里是不会走的，只会走reject，需要在Promise中reject出数据
+//   console.log(1, res)
+// }
+).then((res) => {
+    // then reject具有穿透作用，如果上一级then的reject对应函数没传的话，这里是可以接收到的
+    console.log('res', res);
+}
+  , (err) => {
+  // then里面执行的话，catch中就不会执行
+  console.log('then reject', err);
+  }
+).catch((err) => {
+  // 如果前面所有then里面的reject都没有处理的话，错误信息是会走到catch的
   console.log('catch', err);
 })
 
