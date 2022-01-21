868bae591257e583a31960946258f1b2e6b38b2e
frllk <1402805300@qq.com>
2022-01-21 23:31:18 +0800

    feat: promise asynv await

--- PROMISE/demo6.js
+++ PROMISE/demo6.js
@@ -1,4 +1,7 @@
-const fetch = require('node-fetch')
+// ES5 的模块引入方式
+// const fetch = require('node-fetch')
+// ES6 的模块引入方式
+import fetch from "node-fetch"
 // function getData () {
 //   return new Promise((resolve, reject) => {
 //     fetch('http://study.jsplusplus.com/xiaomi/getXiaomiDatas?phone=true')
@@ -15,27 +18,30 @@ const fetch = require('node-fetch')
 // }
 
 function getData () {
-  // return fetch('http://study.jsplusplus.com/xiaomi/getXiaomiDatas?phone=true')
-  // .then(res => {
-  //   return res.json()
-  // })
-  // .then(res => {
-  //   return res
-  // })
-  // .catch(err => {
-  //   return err
-  // })
-
-  return new Promise((resolve, reject) => {
-    reject('失败了')
+  return fetch('http://study.jsplusplus.com/xiaomi/getXiaomiDatas?phone=true')
+  .then(res => {
+    return res.json()
   })
+  .then(res => {
+    return res
+  })
+  .catch(err => {
+    return err
+  })
+
+  // return new Promise((resolve, reject) => {
+  //   reject('失败了')
+  // })
 }
 
 
 // getData().then(res => {
 //   console.log(res);
 // })
+
+
 /**
+ * @description async中await用法
  * async await
  * async:是个异步函数
  * await：在异步函数中使用的一个操作符
@@ -48,10 +54,17 @@ function getData () {
  * 
  * async 的意思是当前这个异步函数与同一作用域下的程序是异步的关系
  */
-
 async function loadData() {
   const data = await getData()
-  console.log(data);
+  const p1 = await getData(data)
+  const p2 = await getData(p1)
+  const p3 = await getData(p2)
+  const p4 = await getData(p3)
+  console.log(p4);
 }
 
 loadData()
+console.log('Global')
+
+// // async函数返回什么呢？
+// console.log(loadData());
