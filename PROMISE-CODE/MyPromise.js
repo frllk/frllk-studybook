a8880218cf9d6296b3d9f14622144e6ed29d3090
frllk <1402805300@qq.com>
2022-01-23 23:03:41 +0800

    feat： 处理Promise中的异步与多次调用的问题

--- PROMISE-CODE/MyPromise.js
+++ PROMISE-CODE/MyPromise.js
@@ -9,6 +9,11 @@ class MyPromise {
     this.status = PENDING;  // 设置初始状态
     this.value = undefined; // value与reason值不明确，初始为undefined
     this.reason = undefined;
+
+    // 设置两个容器，收集成功或者失败的回调
+    this.onFulfilledCallbacks = []
+    this.onRejectedCallbacks = []
+
     // 每个promise，都有自己的执行器 —— 有自己的resolve，reject。ES6中，如果在外面定义方法，实际上是定义在prototype上面了
     /**
      * @description 定义函数resolve：更改状态
@@ -17,6 +22,8 @@ class MyPromise {
       if (this.status === PENDING) {
         this.status = FULFILLED;
         this.value = value;
+        // 发布
+        this.onFulfilledCallbacks.forEach(fn => fn())
       }  
     }
     /**
@@ -26,6 +33,8 @@ class MyPromise {
       if (this.status === PENDING) {
         this.status = REJECTED;
         this.reason = reason;
+        // 发布
+        this.onRejectedCallbacks.forEach(fn => fn())
       }
     }
 
@@ -45,6 +54,18 @@ class MyPromise {
     // onRejected执行条件
     if (this.status === REJECTED) {
       onRejected(this.reason);
+    }
+
+    if (this.status === PENDING) {
+      // 收集所有的成功的回调、或者是收集所有的失败的回调
+      // 这里要用到发布订阅模式
+      // 订阅的过程
+      this.onFulfilledCallbacks.push(() => {
+        onFulfilled(this.value);
+      })
+      this.onRejectedCallbacks.push(() => {
+        onRejected(this.reason);
+      })
     }
   }
 }
--- PROMISE-CODE/index.js
+++ PROMISE-CODE/index.js
@@ -3,11 +3,22 @@ const MyPromise = require('./MyPromise')
   let promise = new MyPromise((resolve, rejext) => { // executor 执行器 resolve  解决函数  rejext  拒绝函数
   // resolve('success') // 兑现值
   // rejext('error') // 拒绝理由
-  throw new Error('Exception: Error')
-})
+  // throw new Error('Exception: Error')
+    setTimeout(() => {
+      resolve('success')
+    }, 2000);
+  })
+// then方法中的onFulfilled、onRejected必须按照原有的顺序来执行
 promise.then((value) => {
-  console.log('FulFilled: ' + value);
+  console.log('FulFilled1: ' + value);
 }, reason => {
   console.log(1);
-  console.log('Rejected: ', reason);
+  console.log('Rejected1: ', reason);
+})
+
+promise.then((value) => {
+  console.log('FulFilled2: ' + value);
+}, reason => {
+  console.log(1);
+  console.log('Rejected2: ', reason);
 })
