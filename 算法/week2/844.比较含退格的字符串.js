4a37ccbb06c35ac8bd5ffa04094e391574e39377
frllk <1402805300@qq.com>
2022-03-05 00:51:12 +0800

    feat: 844, 946

--- 算法/week2/844.比较含退格的字符串.js
+++ 算法/week2/844.比较含退格的字符串.js
@@ -1,4 +1,12 @@
 /*
+ * @Autor: frllk
+ * @Description: 
+ * @Date: 2022-02-10 11:08:33
+ * @LastEditors: frllk
+ * @LastEditTime: 2022-03-04 23:57:59
+ * @FilePath: \frllk-studybook\算法\week2\844.比较含退格的字符串.js
+ */
+/*
  * @lc app=leetcode.cn id=844 lang=javascript
  *
  * [844] 比较含退格的字符串
@@ -11,7 +19,15 @@
  * @return {boolean}
  */
 var backspaceCompare = function(s, t) {
-
+  return processed(s) === processed(t)
 };
+var processed = function (str) {
+  let stack = []
+  for (ch of str) {
+    if (ch === '#') stack.pop()
+    else stack.push(ch)
+  }
+  return stack.join('')
+}
 // @lc code=end
 
--- 算法/week2/946.验证栈序列.js
+++ 算法/week2/946.验证栈序列.js
@@ -1,4 +1,12 @@
 /*
+ * @Autor: frllk
+ * @Description: 
+ * @Date: 2022-02-10 11:08:34
+ * @LastEditors: frllk
+ * @LastEditTime: 2022-03-05 00:41:04
+ * @FilePath: \frllk-studybook\算法\week2\946.验证栈序列.js
+ */
+/*
  * @lc app=leetcode.cn id=946 lang=javascript
  *
  * [946] 验证栈序列
@@ -10,8 +18,18 @@
  * @param {number[]} popped
  * @return {boolean}
  */
-var validateStackSequences = function(pushed, popped) {
-
+var validateStackSequences = function (pushed, popped) {
+  let cur = 0, stack = [];
+  for (item of pushed) {
+    stack.push(item)
+    while (stack[stack.length - 1] === popped[cur] && stack.length) {
+      // 如果stack中有值，并且最后一个元素与popped[cur]相等时，就从stack中出栈
+      stack.pop()
+      cur++ // 指针向后走一步，找到要输出的下一个数，再次进行对比
+    }
+  }
+  // 栈为空的话，说明pushed数组可以按popped形式输出
+  return !stack.length
 };
 // @lc code=end
 
