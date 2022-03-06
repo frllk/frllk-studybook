1f1b792b5d2a7e2695c9a528d30c388be0d77cd0
frllk <1402805300@qq.com>
2022-03-06 23:56:56 +0800

    feat: 145

--- 算法/week2/145.二叉树的后序遍历.js
+++ 算法/week2/145.二叉树的后序遍历.js
@@ -1,4 +1,12 @@
 /*
+ * @Autor: frllk
+ * @Description: 
+ * @Date: 2022-02-10 11:08:32
+ * @LastEditors: frllk
+ * @LastEditTime: 2022-03-06 23:56:37
+ * @FilePath: \frllk-studybook\算法\week2\145.二叉树的后序遍历.js
+ */
+/*
  * @lc app=leetcode.cn id=145 lang=javascript
  *
  * [145] 二叉树的后序遍历
@@ -17,8 +25,35 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var postorderTraversal = function(root) {
-
+var postorderTraversal = function (root) {
+  // 递归写法
+  // let res = new Array()
+  // return postorderTraversalNode(root, res)
+  // 迭代写法：模拟树 后序遍历：左右中，栈：先进后出，利用栈的特性，逆序的添加进栈
+  // 先把中压入栈，然后把右压入栈，然后把左压入栈，输出的时候先输出左，在输出右，最后输出中
+  // 前后中：
+  // 前
+  // 后
+  // 中
+  let res = []
+  if (!root) return res
+  let stack = [root]
+  while (stack.length) {
+    root = stack.pop()
+    res.unshift(root.val)
+    if (root.left) stack.push(root.left)
+    if (root.right) stack.push(root.right)
+  }
+  return res
 };
+// 递归写法
+var postorderTraversalNode = function (node, res) {
+  if (node) {
+    postorderTraversalNode(node.left, res)
+    postorderTraversalNode(node.right, res)
+    res.push(node.val)
+  }
+  return res
+}
 // @lc code=end
 
