a72aae52bf8515036c92606cf5122552da0e88e2
frllk <1402805300@qq.com>
2022-06-16 23:15:01 +0800

    feat: less

---
+++ webpack/webpack02/src/index.js
@@ -0,0 +1,18 @@
+import css from './style/index.less'
+console.log('Hello webpack')
+
+
+// chunk  chunks  chunkNames
+// chunk  代码片段  一个module对应一个chunk
+// chunks chunk组  {包含至少一个chunk(module)}
+
+// bundle module
+
+// 一个bundle对应一个chunkName(chunks)
+// 一个chunksName(chunks)包含至少一个module（chunk）
+
+
+// loader
+// 假如我们知道webpack只会编译处理js json格式的模块，那么怎么集成样式，图片，vue，jsx等模块呢？
+
+
---
+++ webpack/webpack02/webpack.config.js
@@ -0,0 +1,59 @@
+/*
+ * @Autor: frllk
+ * @Description: 
+ * @Date: 2022-06-07 22:57:30
+ * @LastEditors: frllk
+ * @LastEditTime: 2022-06-16 23:14:24
+ * @FilePath: \frllk-studybook\webpack\webpack02\webpack.config.js
+ */
+// webpack的配置文件
+const { resolve, format } = require("path")
+const htmlwebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin") 
+const miniCssPlugin = require("mini-css-extract-plugin")
+
+// webpack是基于nodeJS
+// 原理就是通过shell脚本在node_modules/.bin⽬录下创建⼀个软链接
+module.exports = {
+  // 入口
+  // spa：单页面应用
+  // mpa：多页面应用   多入口  对应  多出口
+  // entry: "./src/index.js",
+  entry: {
+    // index: modules = [index.js, a.js] = [chunk, chunk] = chunks
+    // index == chunkName
+    index: "./src/index.js", // index.js  a.js index.css
+  },
+  // 出口
+  output: {
+    // 生成的资源存放的位置, 必须是绝对路径
+    path: resolve(__dirname, './build') ,
+    // 生成的资源文件名
+    // filename: 'index.js'  // 占位符 [name]
+    filename: '[name][chunkhash:8].js'  // 占位符 [name]，⽂件名称不要重复
+  },
+  mode: "development", // none（既无开发体验，也无优化） development（开发体验） production(优化)
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [miniCssPlugin.loader, 'css-loader'] // 执行顺序：自后往前
+      },
+      {
+        test: /\.less$/,
+        use: [miniCssPlugin.loader, 'css-loader', 'less-loader'] // 执行顺序：自后往前   //! 不知为何entry为单入口的时候生成的文件没有自动引入css和js
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // 目录清理
+    new miniCssPlugin({
+      filename: "index.css"
+    }), // 样式抽取成独立的文件
+    new htmlwebpackPlugin({
+      template: "./src/public/index.html",
+      filename: "index.html",
+      chunks: ["index"]
+    })
+  ],
+}
