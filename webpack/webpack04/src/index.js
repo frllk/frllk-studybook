8d020f97783ec4e1c178dda68ef988baee89c3a4
frllk <1402805300@qq.com>
2022-06-27 22:31:17 +0800

    feat: 支持vue、react打包

--- webpack/webpack04/src/index.js
+++ webpack/webpack04/src/index.js
@@ -2,7 +2,22 @@
 import "@babel/polyfill";
 
 // 按需载入，减少冗余代码
-const arr = [new Promise(() => { }), new Promise(() => { })]
-arr.map(item => {
-  console.log(item);
-})
+// const arr = [new Promise(() => { }), new Promise(() => { })]
+// arr.map(item => {
+//   console.log(item);
+// })
+
+import React, { Component } from "react";
+import {createRoot } from "react-dom/client"
+// import ReactDom from "react-dom"
+
+class App extends Component {
+  render() {
+    return <div>hello react</div>
+  }
+}
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />)
+// ReactDom.render(<App />, document.getElementById('app'))
